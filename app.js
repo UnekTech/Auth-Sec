69e174e75209a63d90dc281aadf33108776b6acd
UnekTech <augustineocheni10@gmail.com>
2022-07-29 15:12:19 +0100

    Hashing and Salting -bcrypt

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+
 
 const app = express();
 
@@ -19,6 +20,8 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+const saltRounds = 10;
 
 // dataBase-Schema's
 const userSchema = new mongoose.Schema({
@@ -49,38 +52,59 @@ app.get("/register", function (req, res) {
 
 // Post Request
 app.post("/register", function (req, res) {
-    const User = new newUser({
-        username: req.body.username,
-        password: md5(req.body.password)
-    });
 
-    User.save(function (err) {
+    // using bcrypt to salt-hash password
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
         if (err) {
             console.log(err)
         } else {
-            res.render('secrets')
+            const User = new newUser({
+                username: req.body.username,
+                password: hash
+            });
+
+            User.save(function (err) {
+                if (err) {
+                    console.log(err)
+                } else {
+                    res.render('secrets')
+                }
+            });
         }
+
     });
+
+
 });
 
 app.post("/login", function (req, res) {
     const userName = req.body.username;
-    const passWord = md5(req.body.password);
+    const passWord = req.body.password;
 
     newUser.findOne({
         username: userName
-    }, function (err, result) {
-        if (!err) {
-            if (result.password === passWord) {
-                res.render('secrets')
-            } else {
-                res.render('login')
-            }
-        } else {
+    }, function (err, foundUser) {
+        if (err) {
             console.log(err)
+        } else {
+            if (foundUser) {
+                bcrypt.compare(passWord, foundUser.password, function (err, result) {
+                    if (err) {
+                        console.log(err)
+                    } else {
+                        if (result == true) {
+                            res.render('secrets')
+                        }
+                    } 
+                });
+            }
+
         }
+
     });
 });
+
+
 
 // server-feedBack
 app.listen(3000, function () {
